d90603d00d170f60bf44fbadd3e5bc2a8fb6ad17
Dhruv-K-Jain <138356622+Dhruv-K-Jain@users.noreply.github.com>
2023-09-24 13:54:36 +0530

    Videochat on

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,18 @@ import v1 from "./assets/v1.mp4";
 // import DyteProvider from "@dytesdk/react-web-core";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Videochat from "./components/Videochat";
-
+// import defaultimg from "./assets/default.png";
 
 // var i=10;
 // var name="" , email="" , phoneno="" , auth_token="";
 
-class App extends Component{
-  constructor(){
+
+class App extends Component {
+  constructor() {
     super();
-    // this.state={Data};
+    this.state = { auth_token: "", auth_token2: "", meeting_id: "" };
   }
-  
+
   // saveName=(event)=>{
   //   name = event.target.value;
   // }
@@ -58,7 +59,7 @@ class App extends Component{
   //       },
   //       body: '{"name":"Mary Sue","picture":"https://i.imgur.com/test.jpg","preset_name":"group_call_host","custom_participant_id":"host"}'
   //     };
-      
+
   //     fetch('https://api.dyte.io/v2/meetings/bbb88f5f-7300-4284-9ca9-4ba8f22ccdf8/participants', options)
   //       .then(response => response.json())
   //       .then(response => {
@@ -69,17 +70,68 @@ class App extends Component{
   //       .catch(err => console.error(err));
   // }
 
-  render(){
-    return(
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<First />} />
-              <Route path="videochat" element={<Videochat />} />
-              <Route path="*" element={<NoPage />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+  componentDidMount() {
+
+    let ran = Math.random().toString();
+    console.log(typeof (ran));
+    let body = {
+      name: "You name",
+      picture: "https://th.bing.com/th?id=OIP.OesLvyzDO6AvU_hYUAT4IAHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+      preset_name: "group_call_host",
+      custom_participant_id: ran
+    }
+
+    const options1 = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Basic NWE5ZTdhNTEtNjEwNS00NGRjLThlZGEtN2U1OGFlOTA0OGUzOjdiOTc2ZGM2YTY2ZjY5ZWI1NWZi'
+      },
+      body:JSON.stringify(body)
+    };
+
+    fetch('https://api.dyte.io/v2/meetings/bbb88f5f-7300-4284-9ca9-4ba8f22ccdf8/participants', options1)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        this.setState({ auth_token: response.data.token });
+        console.log(this.state.auth_token);
+        this.setState({ meeting_id: response.data.id });
+      })
+      .catch(err => console.error(err));
+
+
+    const options2 = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Basic NWE5ZTdhNTEtNjEwNS00NGRjLThlZGEtN2U1OGFlOTA0OGUzOjdiOTc2ZGM2YTY2ZjY5ZWI1NWZi'
+      },
+      body: '{"name":"Your Name","picture":"https://th.bing.com/th?id=OIP.OesLvyzDO6AvU_hYUAT4IAHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2","preset_name":"group_call_participant","custom_participant_id":"1"}'
+    };
+
+    fetch('https://api.dyte.io/v2/meetings/bbb88f5f-7300-4284-9ca9-4ba8f22ccdf8/participants', options2)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        this.setState({ auth_token2: response.data.token });
+        console.log(this.state.auth_token2);
+      })
+      .catch(err => console.error(err));
+
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<First />} />
+            <Route path="videochat" element={<Videochat authtoken={this.state.auth_token} />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     )
   }
 }
--- src/components/Videochat.jsx
+++ src/components/Videochat.jsx
@@ -2,12 +2,14 @@ import { useEffect } from 'react';
 import { useDyteClient } from '@dytesdk/react-web-core';
 import { DyteMeeting } from '@dytesdk/react-ui-kit';
 
-export default function Videochat() {
+export default function Videochat({authtoken}) {
   const [meeting, initMeeting] = useDyteClient();
+
+  console.log(authtoken);
 
   useEffect(() => {
     initMeeting({
-      authToken: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdJZCI6IjVhOWU3YTUxLTYxMDUtNDRkYy04ZWRhLTdlNThhZTkwNDhlMyIsIm1lZXRpbmdJZCI6ImJiYjg4ZjVmLTczMDAtNDI4NC05Y2E5LTRiYThmMjJjY2RmOCIsInBhcnRpY2lwYW50SWQiOiJhYWFiZDlhYS00OGIwLTRiZjItYWNkZi1hMjNiZTYxZTZhNDciLCJwcmVzZXRJZCI6ImM3NjQwZmJiLTY3OGEtNGY5Yy04YzlmLWU4OTUxOWFjZTMxMiIsImlhdCI6MTY5NTQ3MTUzNiwiZXhwIjoxNzA0MTExNTM2fQ.hqxOdXE-6-5ynVqQY08Mgkc0c2DWFBlbIGHlYyif4I81bMfPFqvGHFD9Wux38djUWBaLuLjhRxrT0NVEDOwQnlpJ5vGzqF5yO1YqQmc4fDmUIgH4eUTyjak0Y9qbPN6FsqGKGu_eH0MrXiXNjK93F5VdYqmuKF0szsUiMlpqzsZo4G2ROvaqbw_jElcCxo7zWVRUfXpVprkb9RL8e6s7d2BiQLOQqQ5qcA1mZpy7cDd8wgf5I7bSolpFrGAQqkmCBM_4jEZjEKvGm4PqZ5DwQNvkHqTz2eSh1Z7_V1bM4nzDtCg9KborfKcoaSfzeppEPBRelAYctMSMGJmmHkhgkQ",
+      authToken: authtoken,
       defaults: {
         audio: false,
         video: false,
