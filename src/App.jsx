db6f5429cf80f1084643bd571ba72134b1efb0ca
Dhruv-K-Jain <138356622+Dhruv-K-Jain@users.noreply.github.com>
2023-09-24 17:40:42 +0530

    Finaltouches

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,8 @@ import Layout from "./components/Layout";
 import NoPage from './components/NoPage';
 import First from './components/First';
 
-import Blog from "./components/Blog";
-import Form from "./components/Form";
-import Data from "./components/User";
-import v1 from "./assets/v1.mp4";
-// import DyteProvider from "@dytesdk/react-web-core";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Videochat from "./components/Videochat";
-// import defaultimg from "./assets/default.png";
-
-// var i=10;
-// var name="" , email="" , phoneno="" , auth_token="";
-
 
 class App extends Component {
   constructor() {
@@ -23,52 +13,6 @@ class App extends Component {
     this.state = { auth_token: "", auth_token2: "", meeting_id: "" };
   }
 
-  // saveName=(event)=>{
-  //   name = event.target.value;
-  // }
-
-  // saveEmail=(event)=>{
-  //   email= event.target.value;
-  // }
-
-  // savePhoneno=(event)=>{
-  //   phoneno= event.target.value;
-  // }
-
-  // submitClick=()=>{
-  //   console.log(name);
-  //   console.log(email);
-  //   console.log(phoneno);
-  //   const detail = {
-  //       id: ++i ,
-  //       name:name,
-  //       email:email,
-  //       phone:phoneno,
-  //   }
-  //   // alert("Submitted Successfully !");
-  //   this.setState({Data : Data.push(detail)});
-  //     name="";
-  //     email="";
-  //     phoneno="";
-  //     console.log(Data);
-  //     const options = {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         Authorization: 'Basic NWE5ZTdhNTEtNjEwNS00NGRjLThlZGEtN2U1OGFlOTA0OGUzOjdiOTc2ZGM2YTY2ZjY5ZWI1NWZi'
-  //       },
-  //       body: '{"name":"Mary Sue","picture":"https://i.imgur.com/test.jpg","preset_name":"group_call_host","custom_participant_id":"host"}'
-  //     };
-
-  //     fetch('https://api.dyte.io/v2/meetings/bbb88f5f-7300-4284-9ca9-4ba8f22ccdf8/participants', options)
-  //       .then(response => response.json())
-  //       .then(response => {
-  //         console.log(response);
-  //         auth_token = response.data.token;
-  //         console.log(auth_token);
-  //       })
-  //       .catch(err => console.error(err));
-  // }
 
   componentDidMount() {
 
--- src/components/First.jsx
+++ src/components/First.jsx
@@ -4,10 +4,6 @@ import './First.css'
 import Blog from "./Blog";
 import Form from "./Form";
 import Data from "./User";
-// import v1 from "./assets/v1.mp4";
-// import DyteProvider from "@dytesdk/react-web-core";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 
 var i = 10;
 var name = "", email = "", phoneno = "";
@@ -74,27 +70,18 @@ class First extends Component {
         <div id="first" className="first w-[99vw] h-screen ">
           <section className="absolute flex justify-center items-center p-16 h-screen">
             <div className=" rounded-3xl ">
-
-              {/* bg-white text-black mix-blend-screen  */}
               <div className='flex justify-center items-center'>
                 <div className='mainfont rounded-3xl w-fit'>
                   <p className="font1 rounded-3xl bg-white text-black mix-blend-screen">AGROGYAN</p>
                 </div>
               </div>
-
               <p className='p-5 text-2xl'>भारतीय किसानों को विज्ञान के साथ सशक्त बनाना। हम अकादमिक लेखों का हिंदी में अनुवाद और सरलीकरण करते हैं, जिससे उन्नत तरीके सुलभ हो जाते हैं। खेती की व्यावहारिक जानकारी के लिए हमसे जुड़ें।</p>
-
-              {/* <div className='flex justify-center items-center'><video className='rounded-t-3xl ' autoPlay loop src="https://video.wixstatic.com/video/11062b_d578b9d4ffba48c68d086ec29fe9e6f0/1080p/mp4/file.mp4" ></video></div> */}
-              {/* <p className='p-5 w-screen h-screen flex justify-center items-center translate-y-[-70%] translate-x-[-20%] absolute font-["Mooli",sans-serif] text-3xl'>Connecting Science with Agriculture.</p> */}
-              {/* <p className="p-2 w-screen h-screen flex justify-center items-center translate-y-[-60%] translate-x-[-25%] absolute pb-0 text-xl">Lorem ipsum dolor sit amet!</p> */}
             </div>
           </section>
         </div>
 
         <div id="slider" className="slider w-screen second bg-[#ff99c8]">
           <div className='buttons flex z-10 absolute w-screen h-100vh'>
-            {/* <button onClick={this.moveLeft}  className='rbtn pt-6 w-[100px] h-[100px]'><img src="leftarrow.png"></img></button> */}
-            {/* <button onClick={this.moveRight} className='lbtn pt-6 w-[100px] h-[100px]'><img src="rightarrow.png"></img></button> */}
           </div>
           <section className=" bg-teal-600">
             <Blog name={"गादी, वी. पी. रेड्डी"}
